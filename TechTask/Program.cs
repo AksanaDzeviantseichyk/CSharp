3ce40ca3ab9bbaa84d1bc2981f28a179f8c23d36
Aksana Dzeviantseichyk <a.dzeviantseichyk@gmail.com>
2023-03-14 22:41:47 +0100

    create a triangle class to check properties of a triangle

---
+++ TechTask/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml.Linq;
+using TechTask;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Triangle triangle = new();
+        triangle.InputCoordinates();
+        triangle.LengthOfSides();
+
+        if (triangle.isExists())
+        {
+            if(triangle.isEquilateral())
+                Console.WriteLine("Triangle IS 'Equilateral'");
+            else 
+                Console.WriteLine("Triangle IS NOT 'Equilateral'");
+
+            if (triangle.isIsosceles())
+                Console.WriteLine("Triangle IS 'Isosceles'");
+            else
+                Console.WriteLine("Triangle IS NOT 'Isosceles'");
+
+            if (triangle.isRight())
+                Console.WriteLine("Triangle IS 'Right'");
+            else
+                Console.WriteLine("Triangle IS NOT 'Right'"); 
+
+            Console.WriteLine($"Perimeter : {triangle.Perimeter()}");
+
+            Console.WriteLine("Parity numbers in range from 0 to triangle perimeter:");
+            for (int i = 0; i <= triangle.Perimeter(); i += 2)
+                Console.WriteLine(i);
+        }
+        else
+            Console.WriteLine("Triangle IS NOT Exists");
+
+    }
+
+    
+}
---
+++ TechTask/Triangle.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechTask
+{
+    public class Triangle
+    {
+        private double Xa;
+        private double Ya;
+        private double Xb;
+        private double Yb;
+        private double Xc;
+        private double Yc;
+        private double lengthAB;
+        private double lengthBC;
+        private double lengthAC;
+
+        public void InputCoordinates()
+        {
+            Console.WriteLine("Enter coordinate x of dot A:");
+            Xa = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter coordinate y of dot A:");
+            Ya = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter coordinate x of dot B:");
+            Xb = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter coordinate y of dot B:");
+            Yb = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter coordinate x of dot C:");
+            Xc = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter coordinate y of dot C:");
+            Yc = Double.Parse(Console.ReadLine());
+
+        }
+
+        public void LengthOfSides()
+        {
+            lengthAB = Math.Sqrt(Math.Pow((Xb - Xa), 2) + Math.Pow((Yb - Ya), 2));
+            Console.WriteLine($"Length of AB is:{lengthAB}");
+            lengthBC = Math.Sqrt(Math.Pow((Xc - Xb), 2) + Math.Pow((Yc - Yb), 2));
+            Console.WriteLine($"Length of BC is:{lengthBC}");
+            lengthAC = Math.Sqrt(Math.Pow((Xc - Xa), 2) + Math.Pow((Yc - Ya), 2));
+            Console.WriteLine($"Length of AC is:{lengthAC}");
+        }
+
+        public bool isExists()
+        {
+            if (lengthAB + lengthBC > lengthAC && lengthAB + lengthAC > lengthBC && lengthBC + lengthAC > lengthAB)
+                return true;
+            return false;
+        }
+
+        public bool isEquilateral()
+        {
+            if (lengthAB == lengthBC && lengthAB == lengthAC && lengthBC == lengthAC)
+                return true;
+            return false;
+        }
+
+        public bool isIsosceles()
+        {
+            if(lengthAB == lengthBC || lengthAB == lengthAC || lengthBC == lengthAC) 
+                return true;
+            return false;
+        }
+
+        public bool isRight()
+        {
+            double delta = 0.01;
+            double maxSide;
+            maxSide = Math.Max(lengthAC, Math.Max(lengthBC, lengthAB));
+            if (maxSide == lengthAC)
+            {
+                if (Math.Abs(Math.Pow(lengthAC,2)-(Math.Pow(lengthBC,2)+Math.Pow(lengthAB,2)))<= delta) return true;
+                return false;
+            }
+            if (maxSide == lengthAB)
+            {
+                if (Math.Abs(Math.Pow(lengthAB, 2) - (Math.Pow(lengthBC, 2) + Math.Pow(lengthAC, 2))) <= delta) return true;
+                return false;
+            }
+            if (maxSide == lengthBC)
+            {
+                if (Math.Abs(Math.Pow(lengthBC, 2) - (Math.Pow(lengthAB, 2) + Math.Pow(lengthAC, 2))) <= delta) return true;
+                return false;
+            }
+            return false;
+        }
+
+        public double Perimeter()
+        {
+            return lengthAB + lengthBC + lengthAC;
+        }
+    }
+}
