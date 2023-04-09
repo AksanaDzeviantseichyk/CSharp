81a81d7d0d46cfc6f61e5a3f7dbb2662d06876c8
Aksana Dzeviantseichyk <a.dzeviantseichyk@gmail.com>
2023-04-10 00:17:39 +0200

    added part 3, edited part 1

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Text;
 using System.Xml.Linq;
 
 class Program
@@ -11,7 +12,7 @@ class Program
             Console.WriteLine("Choose:");
             Console.WriteLine("1 - to determine if a word is a palindrome");
             Console.WriteLine("2 - to swap the values in the array");
-            Console.WriteLine("3 - to change array values ​​to the right and left of the main diagonal");
+            Console.WriteLine("3 - to change array values to the right and left of the main diagonal");
             Console.WriteLine("4 - to play the Game of Life");
             Console.WriteLine("Enter the number of the operation you want to execute:");
             
@@ -19,17 +20,13 @@ class Program
             switch (numberOfOperation)
             {
                 case "1":
-                    Console.WriteLine("Enter the word to determine if a word is a palindrome");
-                    var wordIsPalindrome = Console.ReadLine();
-                    if (isPalindrom(wordIsPalindrome))
-                        Console.WriteLine($"{wordIsPalindrome} - palindrom");
-                    else Console.WriteLine($"{wordIsPalindrome} - not palindrom");
+                    splitPhrase();
                     break;
                 case "2":
                     swapArrayValues();
                     break;
                 case "3":
-                    Console.WriteLine("");
+                    changeArrayValues();
                     break;
                 case "4":
                     Console.WriteLine("");
@@ -46,6 +43,22 @@ class Program
         } while (answer == "Y");
     }
 
+    #region case1
+    static void splitPhrase()
+    {
+        Console.WriteLine("Enter the phrase to determine if a word is a palindrome");
+        var phrase = Console.ReadLine();
+        char[] separators = { ' ', ',', '.', ':', '\t', '–' };
+        String[] words = phrase.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var word in words)
+        {
+            if (isPalindrom(word.ToLower()))
+                Console.Write($"{word} - palindrom ");
+            else Console.WriteLine($"{word} - not palindrom ");
+        }
+        Console.WriteLine();
+        
+    }
     static bool isPalindrom(string word)
     {
         for (int i = 0; i < word.Length / 2; i++)
@@ -53,7 +66,10 @@ class Program
                 return false;
         return true;
     }
-    static void swapArrayValues()
+        #endregion
+
+    #region case2
+        static void swapArrayValues()
     {
         Console.WriteLine("Enter a size of array:");
         int size = int.Parse(Console.ReadLine());
@@ -69,11 +85,57 @@ class Program
             array[array.Length - i - 1] = temp;
         }
         Console.WriteLine("Output array:");
+        printArray(array);
+       
+    }
+    #endregion
+
+    #region case3
+    static void changeArrayValues()
+    {
+        Random rnd = new Random();
+        int arraySize = rnd.Next(1,10);
+        int[,] inputArray = new int[arraySize, arraySize];
+        int[,] outputArray = new int[arraySize, arraySize];
+        for (int i = 0; i < arraySize; i++)
+            for (int j = 0; j < arraySize; j++)
+                inputArray[i, j] = rnd.Next(1,10);
+        Console.WriteLine("Input array:");
+        print2DArray(inputArray);
+        for (int i = 0; i < arraySize; i++)
+            for (int j = 0; j < arraySize; j++)
+                if (j < i)
+                    outputArray[i, j] = 0;
+                else if (j> i) 
+                    outputArray[i, j] = 1;
+                    else outputArray[i, j] = inputArray[i,j];
+        Console.WriteLine("Output array:");
+        print2DArray(outputArray);
+
+    }
+    #endregion
+
+    #region case4
+    static void gameOflife()
+    {
+
+    }
+    #endregion
+
+    public static void printArray<T>(T[] array)
+    {
         foreach (var item in array)
+            Console.Write($"{item}\t");
+        Console.WriteLine();
+    }
+    public static void print2DArray<T>(T[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
         {
-            Console.Write($"{item.ToString()}  ");
+            for (int j = 0; j < matrix.GetLength(1); j++)
+                Console.Write($"{matrix[i, j]}\t");
+            Console.WriteLine();
         }
-        Console.WriteLine("\n");
     }
 
 }
